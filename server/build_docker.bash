aa91bfe3ce0994747ac5f89d5fda23a0ed9780c1
Kirisummer <ediktrikoz@gmail.com>
2023-05-27 00:12:08 +0300

    Add sudo to build and run scripts

--- server/build_docker.bash
+++ server/build_docker.bash
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 source env.bash
 
-docker buildx build -t "$DOCKER_IMAGE_NAME" \
+sudo docker buildx build -t "$DOCKER_IMAGE_NAME" \
     --build-arg OWNER_USER="$OWNER_GRP" \
     --build-arg OWNER_GRP="$OWNER_GRP" \
     --build-arg ADMIN_GRP="$ADMIN_GRP" \
--- server/run.bash
+++ server/run.bash
@@ -5,7 +5,7 @@ source env.bash
 
 ROOT_DIR=/config-server
 mkdir -p "$REPO_DIR" "$USER_DIR" "$PASSWD_DIR"
-docker run -d --rm \
+sudo docker run -d --rm \
     -v "$(realpath $REPO_DIR)":"$ROOT_DIR"/repos \
     -v "$(realpath $USER_DIR)":"$ROOT_DIR"/users \
     -v "$(realpath $PASSWD_DIR)":"$ROOT_DIR"/passwd \
