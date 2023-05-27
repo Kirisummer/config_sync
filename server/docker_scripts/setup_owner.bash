c5f514967eaacecb80dd48add32900b2b18334d7
Kirisummer <ediktrikoz@gmail.com>
2023-05-27 03:31:46 +0300

    Introduce sudo usage, fix validation
    Fix validation checks not aborting the script
    and using print instead of printf.
    Configure sudo according to admin group name.
    Add sudo to user management commands.

--- server/docker_scripts/setup_owner.bash
+++ server/docker_scripts/setup_owner.bash
@@ -3,11 +3,12 @@
 groupadd -- "$USER_GRP"
 groupadd -- "$ADMIN_GRP"
 groupadd -- "$OWNER_GRP"
+groupadd sudoers
 useradd \
     --create-home \
     --base-dir "${ROOT_DIR}/users" \
     --no-user-group \
-    --groups "${USER_GRP},${ADMIN_GRP},${OWNER_GRP}" \
+    --groups "${USER_GRP},${ADMIN_GRP},${OWNER_GRP},sudoers" \
     --shell /usr/bin/git-shell \
     "$OWNER_USER"
 chpasswd <<< "${OWNER_USER}:${OWNER_USER}"
---
+++ server/docker_scripts/setup_sudoers.bash
@@ -0,0 +1,4 @@
+sudoers_file=/etc/sudoers.d/sudo_user_manage
+printf '%%%s ALL=(ALL) !ALL\n'          "$ADMIN_GRP" >> "$sudoers_file"
+printf '%%%s ALL=(ALL) NOPASSWD:USER_ADMIN\n' "$ADMIN_GRP" >> "$sudoers_file"
+chmod 440 "$sudoers_file"
