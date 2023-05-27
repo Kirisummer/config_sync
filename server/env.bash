26f4ff99dd495e237e0de26a91b8fa6e50233637
Kirisummer <ediktrikoz@gmail.com>
2023-05-27 03:34:39 +0300

    Specify parameter categories for env.bash

--- server/env.bash
+++ server/env.bash
@@ -1,11 +1,12 @@
+# image parameters
 DOCKER_IMAGE_NAME=config-server
-
 ROOT_DIR=/config-server
 OWNER_USER=owner
 OWNER_GRP=owner
 ADMIN_GRP=admin
 USER_GRP=user
 
+# host parameters
 REPO_DIR=repos
 USER_DIR=users
 PASSWD_DIR=passwd
