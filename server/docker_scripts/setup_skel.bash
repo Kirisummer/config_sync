32f841fbff64bcd13151a400e23a8ea28e40b262
Kirisummer <ediktrikoz@gmail.com>
2023-05-29 18:14:38 +0300

    Optimize Dockerfile, chmod 700 .ssh in skeleton

--- server/docker_scripts/setup_skel.bash
+++ server/docker_scripts/setup_skel.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 mkdir -p "${ROOT_DIR}/dir_skel/repos" "${ROOT_DIR}/dir_skel/.ssh"
+chmod 700 "${ROOT_DIR}/dir_skel/.ssh"
 ln -s "${ROOT_DIR}/scripts" "${ROOT_DIR}/dir_skel/git-shell-commands"
 
 vars=(ROOT_DIR OWNER_USER OWNER_GRP ADMIN_GRP USER_GRP)
