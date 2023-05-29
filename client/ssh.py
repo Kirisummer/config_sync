a8c29d667c4271a82bf8673c594e4936aecb2d37
Kirisummer <ediktrikoz@gmail.com>
2023-05-29 20:39:09 +0300

    Implement SSH runner

---
+++ client/ssh.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from platform import system
+from subprocess import run
+
+@dataclass(frozen=True)
+class SSHCreds:
+    login: str
+    password: str
+    host: str
+    port: int
+
+@dataclass(frozen=True)
+class SSH:
+    ssh_cmd: tuple[str]
+    input: bytes | None = None
+    def run(self, *command):
+        return run((*self.ssh_cmd, *command), capture_output=True, input=self.input)
+
+    @classmethod
+    def get(cls, ssh_creds):
+        match system():
+            case 'Linux':
+                return LinuxSSH(ssh_creds)
+            case 'Windows':
+                return WinSSH(ssh_creds)
+            case os_name:
+                raise NotImplementedError(f'OS not supported: {os_name}')
+
+class LinuxSSH(SSH):
+    def __init__(self, ssh_creds):
+        super().__init__(
+                (
+                    'sshpass', *get_linux_ssh_cmd(),
+                    '-p', str(ssh_creds.port),
+                    f'{ssh_creds.login}@{ssh_creds.host}'
+                ),
+                input=ssh_creds.password.encode('utf-8')
+        )
+
+class WinSSH(SSH):
+    def __init__(self, ssh_creds):
+        super().__init__((
+                *get_win_ssh_cmd(ssh_creds.password),
+                '-P', str(ssh_creds.port),
+                f'{ssh_creds.login}@{ssh_creds.host}'
+        ))
+
+def get_linux_ssh_cmd():
+    return 'ssh', '-q', '-o', 'StrictHostKeyChecking=no', \
+                        '-o', 'UserKnownHostsFile=/dev/null'
+
+def get_win_ssh_cmd(password):
+    return 'plink', '-ssh', '-pw', password
