51e929a1e9ae7b635f6f123fbf9322f0be536920
Kirisummer <ediktrikoz@gmail.com>
2023-06-03 02:50:56 +0300

    Update UI and UI generating script

--- client/ui/convert.bash
+++ client/ui/convert.bash
@@ -1,6 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 
-rm *.py
-for f in *.ui; do
-    pyside6-uic "$f" > "${f%.*}.py"
+rm *.py || true
+classes=( )
+for ui_file in *.ui; do
+    py_module="${ui_file%.*}"
+    pyside6-uic "$ui_file" > "${py_module}.py"
+    class=$(grep -E '^class ' "${py_module}.py" | awk -F '[ (]' '{ print $2 }')
+    printf 'from .%s import %s\n' "$py_module" "$class" >> __init__.py
+    classes+=( "$class" )
 done
+
+printf '__all__ = [' >> __init__.py
+for class in "${classes[@]}"; do
+    printf '%s, ' "$class" >> __init__.py
+done
+printf ']' >> __init__.py
