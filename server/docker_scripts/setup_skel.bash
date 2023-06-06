7b36e752169e0a8b26b454360895f70724b617aa
Kirisummer <ediktrikoz@gmail.com>
2023-06-06 22:19:40 +0300

    Change user dir permissions to 775

--- server/docker_scripts/setup_skel.bash
+++ server/docker_scripts/setup_skel.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-mkdir -p "${ROOT_DIR}/dir_skel/repos" "${ROOT_DIR}/dir_skel/.ssh"
+mkdir -p "${ROOT_DIR}/dir_skel/_repos" "${ROOT_DIR}/dir_skel/.ssh"
 chmod 700 "${ROOT_DIR}/dir_skel/.ssh"
 ln -s "${ROOT_DIR}/scripts" "${ROOT_DIR}/dir_skel/git-shell-commands"
 
@@ -8,3 +8,6 @@ vars=(ROOT_DIR OWNER_USER OWNER_GRP ADMIN_GRP USER_GRP)
 for var in "${vars[@]}"; do
     printf '%s=%s\n' "$var" "${!var}" >> "${ROOT_DIR}/dir_skel/.ssh/environment"
 done
+
+cd "${ROOT_DIR}/dir_skel"
+ln -s _repos repos
