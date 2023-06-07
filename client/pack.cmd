36e34e553abb1b9fd69c1ae469a2ddfb0a4bc695
Eduard <ediktrikoz@gmail.com>
2023-06-07 09:19:16 +0300

    Update pack.cmd

--- client/pack.cmd
+++ client/pack.cmd
@@ -1,9 +1,10 @@
+del /s /q ssh_bin
 mkdir ssh_bin
-curl -L %plink% -o ssh_bin\plink.exe
-curl -L %git_installer% -o ssh_bin\git_installer.exe
+curl -L https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -o ssh_bin\plink.exe
+curl -L https://github.com/git-for-windows/git/releases/download/v2.41.0.windows.1/PortableGit-2.41.0-64-bit.7z.exe -o ssh_bin\git_installer.exe
 echo Install to ssh_bin\PortableGit - a default option
-ssh_bit\git_installer.exe
-pyinstaller --add-binary=ssh_bin\plink.exe;ssh_bin --add-binary=ssh_bin\PortableGit\cmd\git.exe;ssh_bin --add-data run.cmd main.py
+ssh_bin\git_installer.exe
+pyinstaller --add-binary=ssh_bin\plink.exe;ssh_bin --add-binary=ssh_bin\PortableGit\cmd\git.exe;ssh_bin --add-data run.cmd;. main.py
 
 pushd dist
 move main config_client
