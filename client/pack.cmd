ac90f85f7555eb2cc3be0ee477dca3199abe6c26
Kirisummer <ediktrikoz@gmail.com>
2023-06-08 20:52:09 +0300

    Add --noconsole to windows pyinstaller script

--- client/pack.cmd
+++ client/pack.cmd
@@ -4,7 +4,7 @@ curl -L https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -o ssh_bin\pli
 curl -L https://github.com/git-for-windows/git/releases/download/v2.41.0.windows.1/PortableGit-2.41.0-64-bit.7z.exe -o ssh_bin\git_installer.exe
 echo Install to ssh_bin\PortableGit - a default option
 ssh_bin\git_installer.exe
-pyinstaller --add-binary=ssh_bin\plink.exe;ssh_bin --add-binary=ssh_bin\PortableGit\cmd\git.exe;ssh_bin --add-data run.cmd;. main.py
+pyinstaller --add-binary=ssh_bin\plink.exe;ssh_bin --add-binary=ssh_bin\PortableGit\cmd\git.exe;ssh_bin --add-data run.cmd;. --noconsole main.py
 
 pushd dist
 move main config_client
